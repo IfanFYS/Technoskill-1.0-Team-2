146c831b9f6f0cab538a04150796ebc59799c0aa
kimnzh <152639057+kimnzh@users.noreply.github.com>
2024-07-26 14:52:26 +0700

    Nambahin file front end

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container bg-black w-[360px] h-260px]">Hello, Worlds</div>
+  );
+}
+
+export default App;
