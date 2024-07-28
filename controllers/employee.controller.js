6f4f5cad3c4f9d60005f78ec400d23f447b114e7
GanendrPratama <ganendra1@gmail.com>
2024-07-28 15:36:13 +0700

    Added basic employee and manager functions Added experimental employee functions

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -1,27 +1,120 @@
+// These functions does everything related to employee adding
+
+// This connects to the postgreSQL on connect.js
 const pg = require("../utils/connect");
 
+// This function adds a new entry to the employee table
 exports.addEmployee = async function addEmployee(req, res) {
-  // Insert kode ADD di sini
+  
   try {
     const { name, division, salary } = req.body;
-    const response = await pg.query('INSERT INTO employee (name, division, salary) VALUES ($1, $2, $3) RETURNING *', [name, division, salary]);
+    const response = await pg.query(
+      "INSERT INTO employee (name, division, salary) VALUES ($1, $2, $3) RETURNING *",
+      [name, division, salary]
+    );
 
-    res.status(201).json(response.rows[0]);
+    res.status(201).json(response.rows[0]); // Returns http status code 201.
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json(error.message); // Returns http status code 500 when encountered an error. Indicates an error in the server
   }
 };
 
-exports.getEmployee = async function getEmployee(req, res) {
-  // Insert kode GET di sini
+exports.getAllEmployee = async function getAllEmployee(req, res) {
   try {
-    const response = await pg.query('SELECT * FROM employee');
+    const response = await pg.query("SELECT * FROM employee");
     res.status(200).json(response.rows);
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json(error.message);
   }
 };
 
-exports.getOneEmployee = async function getOneEmployee(req, res) {
-  // Insert kode GET di sini
+// This function searches and shows employees using name
+exports.getEmployeesByName = async function getEmployeesByName(req, res) {
+  try {
+    const { name } = req.body;
+    const response = await pg.query(
+      "SELECT * FROM employee WHERE NAME = ($1)",
+      [name]
+    );
+    
+    if (response.rows.length == 0) {
+      // If the name is not found on the table, the row's length would be zero.
+      // this code will return a http code 404.
+      res.status(404).json({
+        message: "Employee not found"
+      });
+    } else {
+      // If the name is on the table
+      res.status(200).json(response.rows);
+    }
+
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 };
+
+// This function searches and shows employees in a division
+exports.getEmployeesByDivision = async function getEmployeesByDivision (req, res) {
+  try {
+    const { division } = req.body;
+    const response = await.pq.query (
+      "SELECT * FROM employee WHERE DIVISION = ($1)",
+      [division]
+    );
+
+    if (response.rows.length == 0) {
+      res.status(404).json({
+        message:"Division not found or empty"
+      });
+    } else {
+      res.status(200).json(response.rows);
+    }
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+}
+
+// This function searches all employees that has a salary between an interval
+exports.getEmployeesSalaryBetween = async getEmployeesSalaryBetween (req, res) {
+  try {
+    const { min, max } = req.body;
+    const response = await.pq.query (
+      "SELECT * FROM employee WHERE SALARY >= ($1) AND SALARY <= ($2)",
+      [min, max];
+    )
+    if (response.rows.length == 0) {
+      res.status(404).json({
+        message:"No employees between the salary range"
+      });
+    } else {
+      res.status(200).json(response.rows)
+    }
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+}
+
+// This function searches an employee by its id
+exports.getOneEmployeeByID = async function getOneEmployeeByID(req, res) {
+  try {
+    const { id } = req.body;
+
+    const response = await pg.query(
+      "SELECT * FROM employee WHERE ID = ($1)",
+      [id]
+    );
+    
+    if (response.rows.length == 0) {
+      // If the name is not found on the table, the row's length would be zero.
+      // this code will return a http code 404.
+      res.status(404).json({
+        message: "Employee not found"
+      });
+    } else {
+
+    }
+  } catch {
+    // If the name is on the table
+    res.status(200).json(response.rows[0]);
+  }
+}
--- controllers/manager.controller.js
+++ controllers/manager.controller.js
@@ -1,9 +1,13 @@
 const pg = require("../utils/connect");
 
-exports.register = async function addEmployee(req, res) {
+exports.register = async function register(req, res) {
+  // Insert kode REGISTER di sini
   try {
     const { name, password } = req.body;
-    const response = await pg.query('INSERT INTO manager (name, password) VALUES ($1, $2) RETURNING *', [name, password]);
+    const response = await.pq.query (
+      "INSERT INTO manager (name, password) VALUES ($1, $2) RETURNING *",
+      [name, password]
+    );
 
     res.status(201).json(response.rows[0]);
   } catch (error) {
@@ -11,14 +15,17 @@ exports.register = async function addEmployee(req, res) {
   }
 };
 
-exports.login = async function addEmployee(req, res) {
+exports.login = async function login(req, res) {
   // Insert kode LOGIN di sini
   try {
     const { name, password } = req.body;
-    const response = await pg.query('SELECT * FROM manager WHERE name = $1 AND password = $2', [name, password]);
-
+    const response = await pg.query(
+      "SELECT * FROM manager WHERE name = $1 AND password = $2",
+      [name, password]
+    );
+    if (response.rows.length === 0) throw new Error("Failed to login");
     res.status(200).json(response.rows);
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json(error.message);
   }
 };
